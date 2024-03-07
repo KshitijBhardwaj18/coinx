258b91efbac2b0b35607a68a093b35cbf9ce8068
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-03-07 17:45:18 +0530

    Made navbar with modal implemented

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -4,14 +4,23 @@ import {
     createBrowserRouter,
     createRoutesFromElements,
   } from "react-router-dom";
+  import Layout from "../layout";
   
-  
- 
+  import ErrorPage from "./ErrorPage/ErrorPage";
+  import CryptocurrencyDetailsPage from "./CryptocurrencyDetailsPage/CryptocurrencyDetailsPage";
   
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        
+        <Route element={<Layout.Default />}>
+          <Route index element={<Navigate to="/cryptocurrencies/bitcoin" />} />
+          <Route
+            path="/cryptocurrencies/:id"
+            element={<CryptocurrencyDetailsPage />}
+          />
+        </Route>
+  
+        <Route path="*" element={<ErrorPage />} />
       </>
     )
   );
