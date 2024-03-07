ad331dfbf4d5f3097f2e503bdad23a67c90b9f87
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-03-07 17:36:48 +0530

    Setup router and layout

---
+++ src/pages/router.tsx
@@ -0,0 +1,20 @@
+import {
+    Navigate,
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+  } from "react-router-dom";
+  
+  
+ 
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        
+      </>
+    )
+  );
+  
+  export default router;
+  
